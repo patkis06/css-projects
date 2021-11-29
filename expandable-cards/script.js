f3d27ebb62775f818e0f1bb43cf71a420531ad54
patkis <p.kiss8888@gmail.com>
2021-11-29 11:06:17 +0100

    refactoring

--- expandable-cards/script.js
+++ expandable-cards/script.js
@@ -1,6 +1,7 @@
-
+// DOM elements
 const panels = document.querySelectorAll('.panel');
 
+// Events
 panels.forEach(function (panel) {
   panel.addEventListener('click', () => {
     removeActiveClass();
@@ -8,6 +9,7 @@ panels.forEach(function (panel) {
   })
 });
 
+// Functions
 function removeActiveClass() {
   panels.forEach(function (panel) {
     if (panel.classList.contains('active')) {
